f1148cf56a8ac3b40c85fecdfb1bd96917573042
Thesura Siriwardhana <42577570+Thesura@users.noreply.github.com>
2019-09-22 10:46:02 +0530

    DB backup added

--- ComplaintApi/Services/ComplaintRepository.cs
+++ ComplaintApi/Services/ComplaintRepository.cs
@@ -16,7 +16,42 @@ namespace ComplaintApi.Services
         }
         public CompanyMaster GetCompany(string companyId)
         {
-            return _context.CompanyMaster.FirstOrDefault(c => c.CompanyID == companyId);
+            return _context.CompanyMaster.Where(c => c.CompanyID == companyId).FirstOrDefault();
+        }
+
+        public ComplainsMaster getComplains(string companyId, string moduleId, string empId, string priorityId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ComplainsHistory getComplainsHistory(string historyId, string complainId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ModuleMaster getModule(string moduleId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public PriorityMaster getPriority(string priorityId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserMaster getUser(string empId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserCompany getUserCompany(string empId, string companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserModule getUserModule(string empId, string moduleId)
+        {
+            throw new NotImplementedException();
         }
     }
 }
--- ComplaintApi/Services/IComplaintRepository.cs
+++ ComplaintApi/Services/IComplaintRepository.cs
@@ -9,5 +9,20 @@ namespace ComplaintApi.Services
     public interface IComplaintRepository
     {
         CompanyMaster GetCompany(string companyId);
+
+        UserMaster getUser(string empId);
+
+        ModuleMaster getModule(string moduleId);
+
+        PriorityMaster getPriority(string priorityId);
+
+        UserCompany getUserCompany(string empId, string companyId);
+
+        UserModule getUserModule(string empId, string moduleId);
+
+        ComplainsMaster getComplains(string companyId, string moduleId, string empId, string priorityId);
+
+        ComplainsHistory getComplainsHistory(string historyId, string complainId);
+
     }
 }
